215530144a1a54342dabf2ad88c693e262d771b0
dushyant verma <sde.dushyant@gmail.com>
2024-06-21 15:10:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const scoreElement = document.getElementById("score");
+    const clickButton = document.getElementById('click-button');
+    const resetButton = document.getElementById('reset-button');
+
+    let score = 0;
+
+    clickButton.addEventListener('click', () => {
+
+
+        score++;
+        scoreElement.textContent = score;
+    })
+
+    resetButton.addEventListener('click', () => {
+
+
+        score = 0;
+        scoreElement.textContent = score;
+    })
+
+
+
+
+
+
+
+});
